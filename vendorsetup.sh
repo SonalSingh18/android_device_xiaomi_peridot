910a07396e1f1bb19d692b2c441e93f8f349173e
Sonal Singh <sonal.singh.19993@gmail.com>
2024-04-23 15:35:33 -0700

    peridot: Initialize for crDroid 10.x
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/xiaomi-peridot/device_xiaomi_peridot-kernel.git -b udc device/xiaomi/peridot-kernel
+git clone https://github.com/xiaomi-peridot/vendor_xiaomi_peridot.git -b udc vendor/xiaomi/peridot
+git clone https://github.com/xiaomi-peridot/hardware_xiaomi.git -b udc hardware/xiaomi
+git clone https://github.com/xiaomi-peridot/device_xiaomi_peridot-miuicamera.git -b 14 device/xiaomi/peridot-miuicamera
+git clone https://codeberg.org/ramshell688/vendor_xiaomi_peridot-miuicamera.git -b main vendor/xiaomi/peridot-miuicamera
+git clone https://github.com/SonalSingh18/vendor_evolution-priv_keys.git -b 14.0 vendor/lineage-priv/keys
+git clone https://github.com/SonalSingh18/android_packages_apps_Settings.git -b pixel-style vendor/pixel-style
+git clone https://github.com/Chaitanyakm/vendor_bcr.git -b main vendor/bcr
